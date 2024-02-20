baea1d64b608b2a7e65245ba4a8a2fbf95be0a5f
pisha <pisha@hotmail.fr>
2024-02-20 14:56:49 +0100

    [INIT] - burger menu & Searchbar component first init

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,18 +1,43 @@
-import logo from '../../assets/images/logo.png'
+import { useState } from 'react';
+import logo from '../../assets/images/logo.png';
+import Searchbar from './Searchbar';
 
 function Navbar() {
+  const [isMenuVisible, setMenuVisible] = useState(false);
+
+  // Fonction pour basculer la visibilité du menu
+  const toggleMenu = () => {
+    setMenuVisible((prevMenuVisible) => !prevMenuVisible);
+  };
+
   return (
     <div className="navbar-container">
       <div className="navbar-logo-container">
-        <button className='bars-btn'><i className="fa-solid fa-bars fa-2x"></i></button>
+        <button className="bars-btn" onClick={toggleMenu}>
+          <i className="fa-solid fa-bars fa-2x"></i>
+        </button>
         <img src={logo} alt="logo" />
       </div>
-        <div className="navbar-buttons-container">
-            <button className='btn-primary'><i className="fa-solid fa-cart-shopping"></i> Panier</button>
-            <button className='btn-primary'>Connexion</button>
+
+      {isMenuVisible && (
+        <div className="navbar-menu-container">
+          {/* Contenu du menu */}
+          <button className="menu-item">Menu Item 1</button>
+          <button className="menu-item">Menu Item 2</button>
+          <button className="menu-item">Menu Item 3</button>
+          <button className="menu-item">Menu Item 4</button>
         </div>
+      )}
+      <Searchbar/>
+
+      <div className="navbar-buttons-container">
+        <button className="btn-primary">
+          <i className="fa-solid fa-cart-shopping"></i> Panier
+        </button>
+        <button className="btn-primary">Connexion</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
