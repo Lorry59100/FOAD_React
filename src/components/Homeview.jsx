2e673c25086d8935af35952ac90bb8688f592e49
pisha <pisha@hotmail.fr>
2024-02-20 14:55:27 +0100

    [INIT] - img replaced, like system

--- src/components/Homeview.jsx
+++ src/components/Homeview.jsx
@@ -1,5 +1,18 @@
-import tarte from '../assets/images/tarte_au_citron_meringuée.png'
+import { useState } from 'react';
+import { IoMdHeartEmpty, IoMdHeart } from 'react-icons/io';
+import { IconContext } from 'react-icons';
+
 function Homeview() {
+  const [heartStates, setHeartStates] = useState(new Array(6).fill('empty'));
+
+  // Fonction pour basculer entre 'empty' et 'full' pour un cardItem spécifique
+  const toggleHeart = (index) => {
+    setHeartStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = prevStates[index] === 'empty' ? 'full' : 'empty';
+      return newStates;
+    });
+  };
 
   // Fonction pour générer les éléments
   const generateCardItems = () => {
@@ -7,8 +20,15 @@ function Homeview() {
     for (let i = 0; i < 6; i++) {
       cardItems.push(
         <div key={i} className="card-item-container">
-          <img src={tarte} alt={`tarte-${i + 1}`} />
-          <h3>Tarte au citron meringuée maison</h3>
+          <div className="bg-img"></div>
+          <div className="legend">
+            <h3>Tarte au citron meringuée maison</h3>
+            <button className={heartStates[i] === 'empty' ? 'empty-heart' : 'full-heart'} onClick={() => toggleHeart(i)}>
+              <IconContext.Provider value={{ color: 'red', size: '2em' }}>
+                {heartStates[i] === 'empty' ? <IoMdHeartEmpty /> : <IoMdHeart />}
+              </IconContext.Provider>
+            </button>
+          </div>
         </div>
       );
     }
@@ -16,13 +36,11 @@ function Homeview() {
   };
 
   return (
-<div className='homeview-grid-container'>
-    <h1>Découvrez nos nouvelles recettes</h1>
-      <div className="homeview-container">
-        {generateCardItems()}
-      </div>
-</div>    
-  )
+    <div className="homeview-grid-container">
+      <h1>Découvrez nos nouvelles recettes</h1>
+      <div className="homeview-container">{generateCardItems()}</div>
+    </div>
+  );
 }
 
-export default Homeview
+export default Homeview;
