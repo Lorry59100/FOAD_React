998302b10c0081a1493a594e671fc74c5e850699
pisha <pisha@hotmail.fr>
2024-02-20 14:55:50 +0100

    [FIX] - footer design

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,6 +1,11 @@
+import logo from '../../assets/images/logo.png';
 function Footer() {
   return (
-    <div className="footer-container">Footer</div>
+    <div className="footer-container">
+      <h2>2024 - © Cooke Formation CDA : Tous droits réservés</h2>
+      <div className="cutline"></div>
+      <img src={logo} alt="logo" />
+    </div>
   )
 }
 
